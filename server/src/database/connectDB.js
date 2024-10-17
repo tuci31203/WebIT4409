b7604f1bde046e6bd07c3127ffd424bb94db90dd
zeinosnsb <chinhthantoc@gmail.com>
2024-10-17 21:17:56 +0700

    feat: connect to database

--- server/src/database/connectDB.js
+++ server/src/database/connectDB.js
@@ -1,12 +1,14 @@
 import chalk from 'chalk'
 import mongoose from 'mongoose'
 
+import envConfig from '../utils/config.js'
+
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI)
+    const connection = await mongoose.connect(envConfig.MONGO_URI)
     console.log(chalk.green(`MongoDB connected: ${connection.connection.host}`))
   } catch (errors) {
-    throw new Error(`MongoDB connection failed: ${errors.message})`)
+    throw new Error(`MongoDB connection failed: ${errors.message}`)
   }
 }
 
