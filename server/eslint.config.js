ed43843ce51e1e3ebcd51c9ef41d5852c400087b
zeinosnsb <chinhthantoc@gmail.com>
2024-10-18 16:07:42 +0700

    update: change alias

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,8 +1,11 @@
+import babelParser from '@babel/eslint-parser'
+import js from '@eslint/js'
 import prettier from 'eslint-plugin-prettier'
 import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
 
 const config = [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     files: ['**/*.js'],
     plugins: {
@@ -10,9 +13,16 @@ const config = [
       'simple-import-sort': eslintPluginSimpleImportSort
     },
     rules: {
+      ...js.configs.recommended.rules,
       'simple-import-sort/imports': 'warn',
       'simple-import-sort/exports': 'warn',
       'prettier/prettier': ['warn', { usePrettierrc: true }]
+    },
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      parser: babelParser,
+      globals: globals.node
     }
   }
 ]
