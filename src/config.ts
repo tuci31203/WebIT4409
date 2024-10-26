4b06ee4408a95a1508db449584b59669f26aaa24
zeinosnsb <chinhthantoc@gmail.com>
2024-10-26 16:39:54 +0700

    init: config env

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  NODE_ENV: z.string(),
   DATABASE_URL: z.string()
 })
 
 const config = envSchema.safeParse({
+  NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL: process.env.DATABASE_URL
 })
 
