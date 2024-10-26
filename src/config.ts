b927f2e1e6cc76dd37e76b0103ecf67318d10a9b
zeinosnsb <chinhthantoc@gmail.com>
2024-10-26 16:33:24 +0700

    init: config env

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,10 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NEXT_PUBLIC_URL: z.string(),
-  NEXT_PUBLIC_API_ENDPOINT: z.string(),
   DATABASE_URL: z.string()
 })
 
 const config = envSchema.safeParse({
-  NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
-  NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   DATABASE_URL: process.env.DATABASE_URL
 })
 
