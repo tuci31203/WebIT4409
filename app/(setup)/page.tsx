3211603bdf759e38f0e5c03c6cd1bdb8de002099
tuci31203 <chienkt3112@gmail.com>
2024-11-01 13:48:36 +0700

    axios post create chat server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -3,28 +3,24 @@ import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
 
-
 const SetupPage = async () => {
-    const profile = await initialProfile();
-    
-    const server = await db.server.findFirst({
-        where:{
-            members:{
-                some:{
-                    profileId: profile.id
-                }
-            }
-        }
-    });
-    
-    if(server){
-        return redirect(`/server/${server.id}`);
-    }
-    
+  const profile = await initialProfile();
 
-    return (
-        <InitialModal/>
-    );
-}
- 
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+
+  return <InitialModal />;
+};
+
 export default SetupPage;
