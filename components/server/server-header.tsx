370ad0f4971e8ca9197143c2f8c00202934c1016
DuongChinh <chinhlaocai03@gmail.com>
2024-11-04 18:27:37 +0700

    Delete & Leave Server modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -80,6 +80,7 @@ const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("deleteServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Delete Server
@@ -88,6 +89,7 @@ const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("leaveServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
