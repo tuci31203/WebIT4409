a6282934a7ef41b528398cbda1683e3f574792f0
ztrevah <chien03052003@gmail.com>
2024-11-06 13:10:15 +0700

    add api for create, update and delete server

---
+++ src/app/api/servers/[serverId]/route.ts
@@ -0,0 +1,61 @@
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH (
+    req: Request,
+    { params }: { params: Promise<{serverId: string}> }
+) {
+    try {
+        const profile = await currentProfile();
+        if(!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        const { serverId } = await params;
+        const { name, imageUrl } = await req.json();
+        const server = await prisma.server.update({
+            where: {
+                id: serverId,
+                profileId: profile.id
+            },
+            data: {
+                name,
+                imageUrl
+            }
+        });
+
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("[SERVERS_DELETE] ", error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: Promise<{serverId: string}> }
+) {
+    try {
+        const profile = await currentProfile();
+        if(!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        const { serverId } = await params;
+        if(!serverId) {
+            return new NextResponse("Server ID missing", { status: 400 });
+        }
+
+        const server = await prisma.server.delete({
+            where: {
+                id: serverId,
+                profileId: profile.id
+            }
+        });
+        
+        return NextResponse.json(server);
+
+    } catch(err) {
+        console.log("SERVER_ID_DELETE ", err);
+        return new NextResponse("Internal server", { status: 500 }); 
+    }
+}
