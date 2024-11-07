fcae9aeda127d6dc396a84d41f08465d7b30036b
ztrevah <chien03052003@gmail.com>
2024-11-07 13:57:28 +0700

    add api for creating server's invite code, joining server with code and leaving server

---
+++ src/app/api/servers/[serverId]/invite-code/route.ts
@@ -0,0 +1,34 @@
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid"
+
+export async function PATCH (
+    req: Request,
+    { params }: { params: Promise<{serverId: string}>}
+) {
+    try {
+        const profile = await currentProfile();
+        if(!profile) {
+            return new NextResponse("Unauthorized", {status: 401});
+        }
+        const { serverId } = await params;
+        if(!serverId) {
+            return new NextResponse("Server ID missing", {status: 400});
+        }
+        const server = await prisma.server.update({
+            where: {
+                id: serverId,
+                profileId: profile.id,
+            },
+            data: {
+                inviteCode: uuidv4()
+            }
+        });
+        return NextResponse.json(server);
+
+    } catch(error) {
+        console.log("[SERVER_ID]", error);
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
