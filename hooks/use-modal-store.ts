00ffdb53d0d8c323da88cef8423b326d0ac687a0
DuongChinh <chinhlaocai03@gmail.com>
2024-11-20 16:06:53 +0700

    Massage Attachment

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -2,12 +2,14 @@ import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand"
 
 export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel"
-    | "editChannel";
+    | "editChannel" | "messageFile";
 
 interface ModalData {
-    server?: Server
-    channel?: Channel
+    server?: Server;
+    channel?: Channel;
     channelType?: ChannelType;
+    apiUrl?: string;
+    query?: Record<string, any>;
 }
 
 interface ModalStore {
