1ba12ec54662d66b45ee82c2306cf7c0e1341054
tuci31203 <chienkt3112@gmail.com>
2024-12-01 15:11:24 +0700

    fix: hide ChevronDown icon on smaller screens in ServerHeader component

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -33,7 +33,7 @@ const ServerHeader = ({
                     className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
                 >
                     {server.name}
-                    <ChevronDown className="h-5 w-5 ml-auto" />
+                    <ChevronDown className="hidden md:flex h-5 w-5 ml-auto" />
                 </button>
             </DropdownMenuTrigger>
             <DropdownMenuContent
